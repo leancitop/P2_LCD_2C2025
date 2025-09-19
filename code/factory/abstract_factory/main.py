8f2929050988d13da6e51e782859b1e81dd0c2ab
Alejo <alejorazeto@gmail.com>
2025-09-19 16:25:46 -0300

    Paso 2

--- code/factory/abstract_factory/main.py
+++ code/factory/abstract_factory/main.py
@@ -1,9 +1,16 @@
-from .store import NYPizzaStore, ChicagoPizzaStore
+from store import NYPizzaStore, ChicagoPizzaStore
 
 def main():
     ny = NYPizzaStore(); chi = ChicagoPizzaStore()
     ny.order_pizza("cheese")
     chi.order_pizza("clam")
+
+    
+    ny.order_pizza("pepperoni")
+    chi.order_pizza("Pepperoni")
+    
+    ny.order_pizza("veggie")
+    chi.order_pizza("veggie")
 
 if __name__ == "__main__":
     main()
--- code/factory/abstract_factory/pizza.py
+++ code/factory/abstract_factory/pizza.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
-from .ingredients import PizzaIngredientFactory
+from ingredients import PizzaIngredientFactory
 
 class Pizza(ABC):
     def __init__(self, name: str, ing_factory: PizzaIngredientFactory):
         self.name=name; self.f=ing_factory
-        self.dough=None; self.sauce=None; self.cheese=None; self.clam=None
+        self.dough=None; self.sauce=None; self.cheese=None; self.clam=None; self.veggie=None; self.pepperoni=None
     @abstractmethod
     def prepare(self): ...
     def bake(self): print("Bake 25 min at 350")
@@ -23,3 +23,16 @@ class ClamPizza(Pizza):
         print(f"Preparing {self.name}")
         self.dough=self.f.create_dough(); self.sauce=self.f.create_sauce(); self.cheese=self.f.create_cheese(); self.clam=self.f.create_clam()
         print(" ->", self.dough, "/", self.sauce, "/", self.cheese, "/", self.clam)
+
+class VeggiePizza(Pizza):
+    def prepare(self):
+        print(f"Preparing {self.name}")
+        self.dough=self.f.create_dough(); self.sauce=self.f.create_sauce(); self.cheese=self.f.create_cheese(); self.veggie=self.f.create_veggies()
+        print(" ->", self.dough, "/", self.sauce, "/", self.cheese, "/", self.veggie)
+
+class PepperoniPizza(Pizza):
+    def prepare(self):
+        print(f"Preparing {self.name}")
+        self.dough=self.f.create_dough(); self.sauce=self.f.create_sauce(); self.cheese=self.f.create_cheese(); self.pepperoni=self.f.create_pepperoni()
+        print(" ->", self.dough, "/", self.sauce, "/", self.cheese, "/", self.pepperoni)
+
